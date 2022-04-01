e2899805512888454aeb399e36220989db599ad7
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-01 19:52:13 +0300

    UserRefreshToken and UserApp has been created.
    Microsoft.Extensions.Identity.Core has been added

---
+++ AuthServer.Core/Models/UserApp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Models
+{
+    // Bir projeye başlarken mutlaka identity kullanmamız gerekmektedir. İstediğimiz gibi customize edebiliriz.
+    public class UserApp:IdentityUser
+    {
+        public int City { get; set; }
+    }
+}
