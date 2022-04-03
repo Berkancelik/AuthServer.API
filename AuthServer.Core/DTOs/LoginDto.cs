78d52b07955387c8508fd0e707c55a0500a784d7
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-03 14:29:45 +0300

    CreateTokenByRefreshToken fixed

---
+++ AuthServer.Core/DTOs/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.DTOs
+{
+    public class LoginDto
+    {
+
+        // clientId = userName clientSecret= userPassword olarak düşüneceğiz
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        // Revoke etkinliği kırmak anlamını taşımaktadır.
+        //RevokeRefreshToken ele geçirildiği zaman ilgili kullanıcının datasını null a çevirir
+        //yani refresh token da veri çalınmasını önlemek için revokerefreshtoken kullanılmaktadır
+        
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+        Task<Response<ClientLoginDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+
+    }
+}
