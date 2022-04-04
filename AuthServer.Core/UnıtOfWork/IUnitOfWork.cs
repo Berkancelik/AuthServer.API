1159e750e9c9c577605d384290004feea3418443
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-04 10:59:49 +0300

    IUnıtWork has been added

---
+++ AuthServer.Core/UnıtOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.UnıtOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
