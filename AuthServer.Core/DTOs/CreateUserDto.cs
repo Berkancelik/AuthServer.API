8f69635c0851a1a0c6726b1101b67ccb573b0947
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-04 10:31:42 +0300

    IUserService has been completed

---
+++ AuthServer.Core/DTOs/CreateUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.DTOs
+{
+    public class CreateUserDto
+    {
+
+        // Not: ilk aşamada kullanıcıdan birçok veri alınmamalı. Kullanıcıyı yormamak gerekmektedir.
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+
+    // identitiy kütüphanesi ile birlikte  hazır metotlar gelemketedir. O sebeple repository kurmaya gerek kalmamaktadır.
+    // identity üzerinden 3 adet class hazır gelmektedir: bunlar roleManager, userManager, signInManager. Sadece servisi yazmamız yeterli olur
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByNameAsync(string userName);
+    }
+}
