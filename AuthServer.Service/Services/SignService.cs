ab0eee04aef1b576e4eb65169e1173da227e3c22
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-06 00:57:08 +0300

    SecurityKey added

---
+++ AuthServer.Service/Services/SignService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    // asimetrik imzalama birbirine dosya göndermek istediğinde private&public keyler vardır
+    // A                        B 
+    //günümüzdeki haberleşmeler bu yolla yapılmaktadır =>> ssh
+    static class SignService
+    {
+        public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
