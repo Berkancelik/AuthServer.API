3e92b7155ac3f55874745638a9d2ca86f91a929b
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-05 11:17:08 +0300

    UnitOfWorks has been created

---
+++ AuthServer.Data/UnıtOfWork.cs
@@ -0,0 +1,31 @@
+﻿using AuthServer.Core.UnıtOfWork;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data
+{
+    public class UnıtOfWork : IUnitOfWork
+    {
+
+        private readonly DbContext _context;
+
+        public UnıtOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
