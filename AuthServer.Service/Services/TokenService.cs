64fbb30150e98eaba35a3ad549c51359cdc064dd
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-06 01:27:15 +0300

    Token Service fixed

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,51 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOptions _tokenOptions;
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOptions> options)
+        {
+            _userManager = userManager;
+            _tokenOptions = options.Value;
+        }
+
+        private string CreateRefreshToken()
+        {
+            // aşağıda random değer ürettilir. üretilen random değerin bytlerı alınır .
+            // Bu bytle lar yukarıdaki number byte aktarılır
+            // aşağıda trilyonda bir oluşurulması imkansız bir byte oluşturacaktır
+            var numberByte = new Byte[32];
+
+           
+            using var rnd = RandomNumberGenerator.Create();
+            rnd.GetBytes(numberByte);
+            return Convert.ToBase64String(numberByte);
+
+        }
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
