f94af474d7dd0e4924fb7ce515873f99daf33d7e
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-05 11:45:54 +0300

    IMapper has  been created and completed

---
+++ AuthServer.Service/DtoMapper.cs
@@ -0,0 +1,25 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service
+{
+
+    // sadece burada kullacağımız için internal olması yeterlidir. 
+    internal class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            // aşağıdaki işlemde bir dönüştürme uygulanmaktadır. // araştır....
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+
+        }
+
+    }
+}
---
+++ AuthServer.Service/ObjectMapper.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service
+{
+    public static class ObjectMapper
+    {
+        // LazyLoading: Sadeced ihtiyaç olduğu anda yüklemesini sağlamaktadır. 
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DtoMapper>();
+            });
+            return config.CreateMapper();
+
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+
+}
