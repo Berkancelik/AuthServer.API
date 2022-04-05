2a58a3749d7937ebe1a1f4d33f1157a9682c7b6f
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-06 01:14:14 +0300

    Options Pattern added

--- AuthServer.API/Startup.cs
+++ AuthServer.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using SharedLibrary.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,9 @@ namespace AuthServer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Bu olaya ooptionsPattern denmektedir.
+            // aþaðýdaki konfigürasyon ile birlikte genel olarak projede istedeðimiz yerde geçebiliriz"CustomTokenOption"
+            services.Configure<CustomTokenOptions>(Configuration.GetSection("TokenOption"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
---
+++ SharedLibrary/Configuration/CustomTokenOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Configuration
+{
+    public class CustomTokenOptions
+    {
+
+        // appsettings.json yerinde alanları ne olarak belirlediysek burada da onu belirlememiz gerekmektedir.
+        public List<String> Audience { get; set; }
+        public string Issuer { get; set; }
+        public int AccessTokenExpiration { get; set; }
+        public int RefreshTokenExpiration { get; set; }
+        public string SecurityKey { get; set; }
+    }
+}
