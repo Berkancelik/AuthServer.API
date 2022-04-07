53886669dd7e908255a1f2eac1f8e9082333dbd0
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-07 15:54:46 +0300

    TokenService fixed

--- AuthServer.Service/Services/TokenService.cs
+++ AuthServer.Service/Services/TokenService.cs
@@ -7,7 +7,9 @@ using Microsoft.Extensions.Options;
 using SharedLibrary.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,19 +27,39 @@ namespace AuthServer.Service.Services
             _tokenOptions = options.Value;
         }
 
+
         private string CreateRefreshToken()
         {
             // aşağıda random değer ürettilir. üretilen random değerin bytlerı alınır .
             // Bu bytle lar yukarıdaki number byte aktarılır
             // aşağıda trilyonda bir oluşurulması imkansız bir byte oluşturacaktır
             var numberByte = new Byte[32];
-
-           
             using var rnd = RandomNumberGenerator.Create();
             rnd.GetBytes(numberByte);
             return Convert.ToBase64String(numberByte);
 
         }
+        // audienc:  bu token hangi api lere istek yapacağına karşılık gelmektedir.
+        private IEnumerable<Claim> GetClaim(UserApp userApp, List<String> audience)
+        {
+            var userList = new List<Claim> { 
+
+                // nameidentifier : id ye karşılık gelmektedir
+            new Claim(ClaimTypes.NameIdentifier, userApp.Id),
+                new Claim(JwtRegisteredClaimNames.Email, userApp.Email),
+                // identity kütüphanesi gelen token içerisindeki tokenları bulamaz. Bu nedenle bizim role diye 
+                // belirlememiz gerkmektedir.
+                // aşağıdaki gibi bir tanımlama yaptığımızda 
+                new Claim(ClaimTypes.Name, userApp.UserName),
+                // jason a identity vermek için jti tanımlaması yapmamız gerekmektedir.
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                //new Claim("email") == new Claim(JwtRegisteredClaimNames.Email)
+
+        };
+            // token payload olduğunda burada önce bakar
+
+            userList.AddRange(audience.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+    }
         public TokenDto CreateToken(UserApp userApp)
         {
             throw new NotImplementedException();
