b9230cfa0cf4b708681fae6ce1c296127fbcf87f
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-08 15:44:11 +0300

    CreateTokenByRefreshToken has been completed

--- AuthServer.Service/Services/AuthenticationService.cs
+++ AuthServer.Service/Services/AuthenticationService.cs
@@ -82,9 +82,28 @@ namespace AuthServer.Service.Services
 
         }
 
-        public Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken)
+        public async Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken)
         {
-            throw new NotImplementedException();
+            var existRefreshToken = await _userRefreshTokenService.Where(x => x.Code == existRefreshToken).SingleOrDefaultAsync();
+            if (existRefreshToken==null)
+            {
+                return Response<TokenDto>.Fail("Refresh token not found", 404, true);
+            }
+            var user = await _userManager.FindByIdAsync(existRefreshToken.UserId);
+            if (user==null)
+            {
+                return Response<TokenDto>.Fail("User Id not found", 404, true);
+            }
+
+            var tokenDto = _tokenService.CreateToken(user);
+            existRefreshToken.Code = tokenDto.RefreshToken;
+            existRefreshToken.Expiration = tokenDto.RefreshTokenExpiration;
+
+            await _unitOfWork.CommitAsync();
+
+            return Response<TokenDto>.Success(tokenDto, 200);
+
+
         }
 
         public Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken)
